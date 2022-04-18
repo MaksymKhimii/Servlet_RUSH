51cfdf82de58461d87cbe63dc66ac8bb01c859a9
Maksym Khimii <meizovicmaks@gmail.com>
2022-04-18 15:41:28 +0300

    написал документацию для ReportDAO (5/6)

--- src/main/java/db/ReportDAO.java
+++ src/main/java/db/ReportDAO.java
@@ -15,23 +15,26 @@ public class ReportDAO {
     private static final String PASSWORD = "makskhimiy24112003";
 
     /** RU: метод для создания соединения между базой данных и программой
-     * ENG: method to create connection between database and program */
+     *  ENG: method to create connection between database and program
+     * @return Connection
+     */
     public Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
         Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/epam?serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "root", "1234567h");
         con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-        // con.setAutoCommit(true);
-
         return con;
     }
 
-    /**метод для проверки прошли ли сутки с предыдущего запроса отчета*/
+
+    /** RU: метод для проверки прошли ли сутки с предыдущего запроса отчета
+     * ENG: method for checking if a day has passed since the previous report request
+     * @return status
+     */
     public static boolean AllowToReport() throws ClassNotFoundException, SQLException, ParseException {
         boolean status=false;
         SimpleDateFormat formattedDate = new SimpleDateFormat("yyyy-MM-dd");
@@ -50,7 +53,6 @@ public class ReportDAO {
             java.util.Date dbase=formattedDate.parse(basedate);
             System.out.println("dbase: "+dbase);
             if(dbase.compareTo(dnow)<0){
-                //тогда правда
                 status = true;
             } else if(dbase.compareTo(dnow)==0){
                 status=false;
@@ -59,10 +61,13 @@ public class ReportDAO {
         return status;
     }
 
-    /**проверка на налачие хотя бы одного нулевого отчета*/
-    public static boolean checkReport() throws SQLException, ClassNotFoundException {
-        boolean answer = false;
 
+    /** RU: проверка на налачие хотя бы одного нулевого отчета в базе данных
+     * ENG: check for at least one null report in databases
+     * @return status
+     */
+    public static boolean checkReport() {
+        boolean answer = false;
         try {
             String QUERY = "SELECT * FROM mydbtest.report;";
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -77,18 +82,21 @@ public class ReportDAO {
         return answer;
     }
 
-    /** сохранение нового отчета*/
-    public static void addReport(int QuantityOfReceipts, int LastReceiptId, double TotalSum) throws SQLException, ClassNotFoundException {
-        Date date = new Date();
+
+    /** RU: сохранение нового отчета
+     * ENG: saving a new report
+     * @param QuantityOfReceipts number of checks created in the last day
+     * @param LastReceiptId last receipt id
+     * @param TotalSum total sum for all these checks
+     */
+    public static void addReport(int QuantityOfReceipts, int LastReceiptId, double TotalSum) throws ClassNotFoundException {
         SimpleDateFormat formatForDateNow = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         Date datenow0 = new Date();
         String Time= formatForDateNow.format(datenow0);
         try {
             String ADD_Product = "INSERT INTO mydbtest.report (quantityOfReceipts, lastReceiptId, time , totalSum) VALUES(?, ?, ?, ?);";
-
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
             PreparedStatement ps = con.prepareStatement(ADD_Product);
             ps.setString(1, String.valueOf(QuantityOfReceipts));
             ps.setString(2, String.valueOf(LastReceiptId));
@@ -100,9 +108,12 @@ public class ReportDAO {
         }
     }
 
-    /** метод получает данные всех суточных отчетов */
+    /** RU: метод получает данные всех суточных отчетов
+     * ENG: method gets data from all daily reports
+     * @return report
+     */
     public static ArrayList<Report> getAllReports(){
-        ArrayList<Report> reports= new ArrayList<Report>(){};
+        ArrayList<Report> reports= new ArrayList<>(){};
         String Query = "SELECT idreport, quantityOfReceipts, lastReceiptId, time, totalSum FROM mydbtest.report;";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
